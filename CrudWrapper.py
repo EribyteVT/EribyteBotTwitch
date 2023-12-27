307f1f8aa855a7346cdc24f8f5944fd053e06820
EribyteVT <eribytevt@gmail.com>
2023-12-26 19:09:02 -0600

    Initial bot

---
+++ CrudWrapper.py
@@ -0,0 +1,304 @@
+import requests 
+from math import floor
+import time
+import random 
+import datetime
+import re 
+import pytz 
+
+class CrudWrapper:
+    def __init__(self,env):
+        self.env = env
+        if(env == "PROD"):
+            self.urlBase = 'http://10.0.0.6:8080'
+
+        elif(env == "LOCAL"):
+            self.urlBase = 'http://127.0.0.1:8080'
+            
+        elif(env == "DEV"):
+            self.urlBase = 'https://crud.eribyte.net'
+
+        else:
+            raise Exception("ERROR, ENV NOT SET")
+    def getLevelFromXp(self,xp):
+        """
+        THE 2 FUNCTIONS FOR XP:
+        0-50: 0.04x^3 + 0.8x^2 + 2x
+        50+: 400(x-32.25)
+        """
+        #we have 2 different functions for xp, rollover xp is second function
+        rollover_xp=0
+
+        #split into 2 diff xp graphs
+        if(xp>7100):
+            rollover_xp = xp-7100
+            xp=7100
+        
+        highestLevel = 0
+
+        for i in range(51):
+            xp_for_i = .04 * (i**3) + .8 * (i**2) + 2*i
+            if xp >= xp_for_i:
+                highestLevel = i
+
+        level = highestLevel
+
+        #always rollover, it's either 0 or something
+        level += floor((rollover_xp)//400)
+
+        return level
+
+    def getConnectedAccounts(self,id):
+        url = self.urlBase + '/getConnections/'+str(id)
+        data = requests.get(url)
+
+        #return new discord account
+        if(data.text is None or data.text ==""):
+            return None
+        else:
+            json = data.json()
+            return json
+        
+    def getConnectedAccountsTwitch(self,id):
+        url = self.urlBase + '/getTwitchConnections/{twitchId}/'+str(id)
+        data = requests.get(url)
+
+        #return new discord account
+        if(data.text is None or data.text ==""):
+            return None
+        else:
+            json = data.json()
+            return json
+    
+    def getUserTotalXP(self,accounts):
+        xp = 0
+
+        did_discord = False
+        
+        for account in accounts:
+            if(not did_discord):
+               xp += self.getDataFromDiscordId(account['serviceId'])['xp']
+               did_discord = True 
+
+            match(account['serviceName']):
+                case "twitch":
+                    xp += self.getDataFromTwitchdId(account['serviceId'])['xp']
+                case "youtube":
+                    xp += self.getDataFromYoutubeId(account['serviceId'])['xp']
+                case _:
+                    raise Exception("SHIT'S FUCKED, NOT IMPLEMENTED YET")
+            
+        return xp
+
+        
+    def getXpFromAccounts(self,accounts):
+        data_list = []
+        
+        for account in accounts:
+            match(account['serviceName']):
+                case "twitch":
+                    account_xp = self.getDataFromTwitchdId(account['serviceId'])
+                case "youtube":
+                    account_xp = self.getDataFromYoutubeId(account['serviceId'])
+                case _:
+                    raise Exception("SHIT'S FUCKED, NOT IMPLEMENTED YET")
+            
+            data_list.append(account_xp)
+        
+        return data_list
+
+    ############## ADD SERVICE TO DISCORD ACCOUNT #########################################
+
+    def addTwitchToDiscord(self,discord_id, twitch_id):
+        #data for update
+        data = {"discordId":discord_id,"serviceId":twitch_id}
+
+        #data to send update to
+        url = self.urlBase + '/addConnectionDiscord/twitch'
+
+        #send request
+        request = requests.post(url,json=data)
+
+        return request
+
+    def addYoutubeToDiscord(self,discord_id, youtube_id):
+        data = {"discordId":discord_id,"serviceId":youtube_id}
+
+        #data to send update to
+        url = self.urlBase + '/addConnectionDiscord/youtube'
+
+        #send request
+        request = requests.post(url,json=data)
+
+        return request
+
+    #################### CHECK SERVICE CONNECTED ##########################################
+
+    #True if connected, false if not
+    def twitchConnected(self,id):
+        url = self.urlBase + '/getConnections/twitch/'+str(id)
+
+        #get data
+        data = requests.get(url)
+
+        if(data.text is None or data.text ==""):
+            return False
+        else:
+            return True
+        
+    def youtubeConnected(self,id):
+        url = self.urlBase + '/getConnections/youtube/'+str(id)
+
+        #get data
+        data = requests.get(url)
+
+        if(data.text is None or data.text ==""):
+            return False
+        else:
+            return True
+
+
+    ################# GET DATA FROM SERVICE ####################################################
+
+    def getDataFromDiscordId(self,id):
+        #ALSO ADDS USER BTW FUTURE ERIBYTE
+        url = self.urlBase + '/getbyId/discord/'+str(id)
+
+        #get data
+        data = requests.get(url)
+
+        #if none, add user
+        if(data.text is None or data.text ==""):
+            data = self.addXpbyDiscordId(0,id,False)
+        else:
+            data = data.json()
+
+
+        #return the data
+        return data
+
+    def getDataFromTwitchdId(self,id):
+        #ALSO ADDS USER BTW FUTURE ERIBYTE
+        url = self.urlBase + '/getbyId/twitch/'+str(id)
+
+        #get data
+        data = requests.get(url)
+
+        if(data.text is None or data.text ==""):
+            return  self.addXpbyTwitchId(0,id,False)
+        else:
+            return data.json()  
+
+    def getDataFromYoutubeId(self,id):
+        #ALSO ADDS USER BTW FUTURE ERIBYTE
+        url = self.urlBase + '/getbyId/youtube/'+str(id)
+
+        #get data
+        data = requests.get(url)
+
+        if(data.text is None or data.text ==""):
+            return self.addXpbyYoutubeId(0,id,False)
+        else:
+            return data.json()
+
+
+    ######################### GET ASSOCIATED #######################################################
+        
+    def getAssociatedFromTwitch(self,id):
+        url = self.urlBase + '/GetAllAccountsAssociated/twitch/'+str(id)
+
+        #get data
+        data = requests.get(url)
+
+        #if none, add user
+        if(data.text is None or data.text ==""):
+            data = [self.addXpbyTwitchId(0,id,False)]
+        else:
+            data = data.json()
+
+
+        #return the data
+        return data
+
+
+    ################ ADD XP #######################################################
+
+    def addXpbyDiscordId(self,xp,id,update):
+        #get current time in okay enough format
+        currentTime = str(time.time())[:-4]
+        currentTime = ''.join(currentTime.split('.'))
+
+        if(len(currentTime)==12):
+            currentTime+='0'
+        
+        #data for update
+        data = {"id":id,"xp":xp,"updateTime":update,"newTime":currentTime}
+
+        #data to send update to
+        url = self.urlBase + '/update/discord'
+
+        #send request
+        request = requests.post(url,json=data).json() 
+
+        return request
+
+    def addXpbyTwitchId(self,xp,id,update):
+        #get current time in okay enough format
+        currentTime = str(time.time())[:-4]
+        currentTime = ''.join(currentTime.split('.'))
+
+        if(len(currentTime)==12):
+            currentTime+='0'
+        
+        #data for update
+        data = {"id":id,"xp":xp,"updateTime":update,"newTime":currentTime}
+
+        #data to send update to
+        url = self.urlBase + '/update/twitch'
+
+        #send request
+        request = requests.post(url,json=data).json() 
+
+        return request
+
+    def addXpbyYoutubeId(self,xp,id,update):
+        #get current time in okay enough format
+        currentTime = str(time.time())[:-4]
+        currentTime = ''.join(currentTime.split('.'))
+
+        if(len(currentTime)==12):
+            currentTime+='0'
+        
+        #data for update
+        data = {"id":id,"xp":xp,"updateTime":update,"newTime":currentTime}
+
+        #data to send update to
+        url = self.urlBase + '/update/youtube'
+
+        #send request
+        request = requests.post(url,json=data).json() 
+
+        return request
+
+    def enoughTime(self,lastTime):
+        #get current time
+        currentTime = str(time.time())[:-4]
+        currentTime = ''.join(currentTime.split('.'))
+
+        if(len(currentTime)<13):
+            currentTime = currentTime.ljust(13,'0')
+        
+        #how we tell if enough time has passed (a random 7-17 mins)
+        bodgeTime = (12 + random.randint(-5,5))*60000 
+
+        #real complicated logic to convert dates (man I fuckin hate dates)
+        lastTime = datetime.datetime.timestamp(parse_timestamp(lastTime[:-5]))*1000
+
+        #if it's been 7-17 minutes past the last update, update
+        if(int(currentTime) > int(lastTime)+bodgeTime):
+            return True 
+        
+        return False
+    
+def parse_timestamp(timestamp):
+    return datetime.datetime(*[int(x) for x in re.findall(r'\d+', timestamp)],tzinfo=pytz.utc)
---
+++ init_oath.py
@@ -0,0 +1,100 @@
+from twitchAPI.twitch import Twitch
+from twitchAPI.oauth import UserAuthenticationStorageHelper
+from twitchAPI.type import AuthScope, ChatEvent
+from twitchAPI.helper import first
+from twitchAPI.chat import Chat, EventData, ChatMessage, ChatSub, ChatCommand
+import asyncio
+from CrudWrapper import CrudWrapper
+import random
+from Secrets import * 
+
+crudWrapper = CrudWrapper("LOCAL")
+twitch = None
+
+async def on_ready(ready_event: EventData):
+    await ready_event.chat.join_room("EribyteVT")
+    # you can do other bot initialization things in here
+
+async def on_message(msg: ChatMessage):
+    id = msg.user.id
+
+    data = crudWrapper.getAssociatedFromTwitch(id)
+
+    #not in db at all
+    if(data == None or data == [] or data[0] == None):
+        data = [crudWrapper.getDataFromTwitchdId(id)]
+
+    twitch = None
+
+    total_xp = 0
+
+    for account in data:
+        if(account['serviceName'] == 'twitch'):
+            twitch = account
+        total_xp += account['xp']    
+
+    levelBefore = crudWrapper.getLevelFromXp(total_xp)
+
+    #if new account or time between messages is enuf, add xp
+    if(twitch['lastMessageXp']==None or crudWrapper.enoughTime(twitch['lastMessageXp'])):
+        xp_awarded_amount = random.randint(1,5)
+        data = crudWrapper.addXpbyTwitchId(xp_awarded_amount,id,True)
+        total_xp += xp_awarded_amount
+
+    levelAfter = crudWrapper.getLevelFromXp(total_xp)
+        
+    if(levelAfter > levelBefore):
+        await msg.chat.send_message("eribytevt",f"Congratulations to @{msg.user.name} for reaching level {levelAfter}!!!!")
+
+async def ping(cmd: ChatCommand):
+    await cmd.reply("!discord")
+
+async def getLevel(cmd: ChatCommand):
+    id = cmd.user.id
+
+    data = crudWrapper.getAssociatedFromTwitch(id)
+
+    total_xp = 0
+
+    for account in data:
+        total_xp += account['xp']    
+
+    level = crudWrapper.getLevelFromXp(total_xp)
+
+    await cmd.reply(f"Level: {level}    XP: {total_xp}")
+
+async def raid_shoutout(raid_event: dict):
+    print(dict)
+    user_id = raid_event['tags']['user-id']
+    print(user_id)
+    
+    await twitch.send_a_shoutout("946740776",user_id,"1004092875")
+
+async def run():
+    global twitch
+    TARGET_SCOPES = [AuthScope.MODERATOR_READ_FOLLOWERS,AuthScope.CHANNEL_MANAGE_REDEMPTIONS,AuthScope.USER_READ_SUBSCRIPTIONS,AuthScope.MODERATOR_READ_CHATTERS,AuthScope.MODERATOR_MANAGE_SHOUTOUTS,AuthScope.CHAT_READ,AuthScope.CHANNEL_MANAGE_RAIDS,AuthScope.MODERATOR_MANAGE_SHOUTOUTS,
+                     AuthScope.CHAT_EDIT,AuthScope.MODERATOR_MANAGE_SHOUTOUTS]
+
+    twitch = await Twitch(APP_ID, APP_SECRET)
+    helper = UserAuthenticationStorageHelper(twitch, TARGET_SCOPES)
+    await helper.bind()
+
+    chat = await Chat(twitch)
+
+    chat.register_event(ChatEvent.MESSAGE, on_message)
+    chat.register_event(ChatEvent.READY, on_ready)
+    chat.register_event(ChatEvent.RAID,raid_shoutout)
+
+    chat.register_command('ping', ping)
+    chat.register_command('getLevel', getLevel)
+
+    chat.start()
+
+    try:
+        input('press ENTER to stop\n')
+    finally:
+        # now we can close the chat bot and the twitch api client
+        chat.stop()
+        await twitch.close()
+
+asyncio.run(run())
